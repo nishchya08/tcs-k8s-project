77679c8a48650eacb6bc1f6cf1b8b6be22d3ee0d
Ramesh A <ramesharpu@gmail.com>
2025-07-21 18:13:02 +0530

    initial commit

---
+++ src/main/java/com/tcs/my_project/MyProjectApplication.java
@@ -0,0 +1,20 @@
+package com.tcs.my_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProjectApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello() {
+		return "<h1>Hello There!!! Welcome to my project</h1>";
+	}
+}
